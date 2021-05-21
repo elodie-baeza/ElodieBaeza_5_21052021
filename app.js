f300c03da92d605abc07ced7e49b50ecb47fd141
elodie baeza <elodie.baeza@gmail.com>
2021-05-21 21:35:06 +0200

    sass 7.1

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-import
+import {recipes} from './recipes.js';
+console.table(recipes);
+// recipes.map(item => console.log(item +1))
---
+++ readme.md
@@ -0,0 +1,2 @@
+# ElodieBaeza_5_21052021
+[githubpage](https://elodie-baeza.github.io/ElodieBaeza_4_21052021/)
