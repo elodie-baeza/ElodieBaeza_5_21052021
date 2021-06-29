d8c57c7d195fc061bfbba5a5ddb67692d0ddfc8b
elodie baeza <elodie.baeza@gmail.com>
2021-06-29 13:30:38 +0200

    if "s" is last character of word, remove it

--- js/utils/formatString.js
+++ js/utils/formatString.js
@@ -1,3 +1,10 @@
 export function formatString(str) {
-    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase().trim();
+    //supprime les espaces
+    str = str.trim();
+    //si dernière lettre est "s" alors le supprimer
+    if (str.substring(str.length-1) == 's') {
+       str = str.substr(0, str.length-1)
+    }
+    //renvoie une chaine en majuscule et sans accents
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase();
 }
