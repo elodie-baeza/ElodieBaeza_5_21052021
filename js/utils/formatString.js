adc69287e0248471c75a94317f3a85ba4d8ddf6d
elodie baeza <elodie.baeza@gmail.com>
2021-06-30 10:43:53 +0200

    Revert "if "s" is last character of word, remove it"
    This reverts commit d8c57c7d195fc061bfbba5a5ddb67692d0ddfc8b.

--- js/utils/formatString.js
+++ js/utils/formatString.js
@@ -1,10 +1,3 @@
 export function formatString(str) {
-    //supprime les espaces
-    str = str.trim();
-    //si dernière lettre est "s" alors le supprimer
-    if (str.substring(str.length-1) == 's') {
-       str = str.substr(0, str.length-1)
-    }
-    //renvoie une chaine en majuscule et sans accents
-    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase();
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase().trim();
 }
