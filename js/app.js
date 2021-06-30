67affbbb0fefe4a861e76ecd127a65cf8a055e56
elodie baeza <elodie.baeza@gmail.com>
2021-06-30 10:42:57 +0200

    remove console.log

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 import eventKeyupSearchFilter from './listener/eventKeyupSearchFilter.js';
-import { recipes } from './data/recipes.js';
+// import { recipes } from './data/recipes.js';
 import eventKeyMainSearch from './listener/eventKeyMainSearch.js';
 import RecipesClean from './data/RecipesClean.js';
 import SearchServices from './search/searchServices/SearchServices.js';
 
-console.log(recipes);
-
 export const recipesClean = RecipesClean.clean();
-console.log(recipesClean);
+// console.log(recipesClean);
 
 export const searchServices = new SearchServices();
 searchServices.launchSearch();
