9be87d8548d45f3dcd73029eee22d622ff6775af
elodie baeza <elodie.baeza@gmail.com>
2021-06-30 10:42:26 +0200

    remove last character "S" of word in main input

--- js/search/searchServices/SearchByMainInput.js
+++ js/search/searchServices/SearchByMainInput.js
@@ -4,15 +4,23 @@ import { tableOfWords } from "../../utils/tableOfWords.js";
 export default class SearchByMainInput {
     static research(mainInput, resultList) {
         this.recipesResultList = new Set();
-        // this.recipes = recipesClean;
+
         this.arrayMainInput = tableOfWords(mainInput);
 
+        //si dernière lettre est "s" alors le supprimer
+        this.arrayMainInput.forEach((word, index) => {
+            if (word.substring(word.length-1) == 'S') {
+                this.arrayMainInput[index] = word.substr(0, word.length-1)
+            }
+        });
+        console.log(this.arrayMainInput)
         resultList.forEach(recipe => {
-            if (this.arrayMainInput.every(x => recipe.name.includes(x) || recipe.description.includes(x) || recipe.ingredientsString.includes(x))) {
+            if (this.arrayMainInput.every(x => recipe.name.includes(x)
+            || recipe.description.includes(x)
+            || recipe.ingredientsString.includes(x))) {
                 this.recipesResultList.add(recipesClean[recipe.id-1]);
             }
         })
-
         return this.recipesResultList;
     }
 }
